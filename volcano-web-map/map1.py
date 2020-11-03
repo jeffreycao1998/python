32053a5c8db0a7fa76b19b2301c318d834f50fb2
jeffreycao1998 <jeffreycao1998@hotmail.com>
2020-11-03 11:48:04 -0500

    added layer control

--- volcano-web-map/map1.py
+++ volcano-web-map/map1.py
@@ -15,20 +15,29 @@ def color_producer(elevation):
   else:
     return 'red'
 
-map = folium.Map(location=[38.58, -98], zoom_start=6, tiles='Stamen Terrain')
+map = folium.Map(location=[38.58, -113], zoom_start=5, tiles='Stamen Terrain')
 
 # feature group
-fg = folium.FeatureGroup(name='My Map')
+fgv = folium.FeatureGroup(name='Volcanoes')
 
 for lt, ln, el, name in zip(lat, lon, elev, name):
   html = f"Volcano name:<a href='https://www.google.com/search?q={name}' target='_blank'> {name}</a><br>Height: {int(el)}m"
   iframe = folium.IFrame(html=html, width=200, height=100)
 
-  fg.add_child(folium.CircleMarker(location=[lt, ln], popup=folium.Popup(iframe), 
+  fgv.add_child(folium.CircleMarker(location=[lt, ln], popup=folium.Popup(iframe), 
   radius=6, weight=1, color='grey', fill_color=color_producer(el), fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+fgp = folium.FeatureGroup(name='Population')
 
-map.add_child(fg)
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: { 
+  'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 
+              'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 
+              'red'
+}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save('Map1.html')
