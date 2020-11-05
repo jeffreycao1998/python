e462490d51472aa39bcce568dac3354fda3919ff
jeffreycao1998 <jeffreycao1998@hotmail.com>
2020-11-05 15:17:51 -0500

    created different routes

--- personal-website/script1.py
+++ personal-website/script1.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return 'Website content goes here!'
+  return 'Homepage here!'
+
+@app.route('/about/')
+def about():
+  return 'About content goes here!'
 
 if __name__=='__main__':
   app.run(debug=True)
