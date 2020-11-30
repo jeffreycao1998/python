7e254fee9d7c20ceae6a1783fc377617cd2b2cc3
jeffreycao1998 <jeffreycao1998@hotmail.com>
2020-11-29 21:01:27 -0500

    added form to website

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,10 +1,15 @@
 from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from meetings.models import Meeting, Room
+
 # Create your views here.
 def welcome(request):
-  return HttpResponse("Welcome to the Meeting Planner!")
+  return render(request, "website/welcome.html", {
+    "meetings": Meeting.objects.all(),
+  })
 
 def date(request):
   return HttpResponse("This page was served at " + str(datetime.now()))
